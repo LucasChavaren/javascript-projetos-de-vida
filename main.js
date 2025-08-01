2ab056dec9c3821c3e642608a7ef6fbeff1e12a7
LucasChavaren <lucas.chavaren.lima@escola.pr.gov.br>
2025-08-01 07:46:17 -0300

    setinterval

--- main.js
+++ main.js
@@ -12,7 +12,12 @@ for(let i = 0; i< botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 - new Date("2025-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2026-02-05T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
@@ -28,7 +33,20 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
+if(tempoFinal > 0){
 return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
-
-
+}else{
+    return "Prazo Finalizado."
 }
+
+function atualizaCronometro(){
+    for (let i = 0; i > contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
